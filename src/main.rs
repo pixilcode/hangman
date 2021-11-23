05f032951a583aa6e0f7777d60cacfc53c8dfafb
Brendon Bown <brendon.bown@gmail.com>
2021-11-23 09:23:14 -0700

    Added the word dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use std::collections::HashSet;
+
 mod game;
 
 fn main() {
-    println!("Hello, world!");
+    let dictionary = construct_dictionary();
+
+    let game = game::HangmanGame::new(dictionary);
+}
+
+fn construct_dictionary() -> HashSet<String> {
+    let mut dictionary = HashSet::new();
+
+    dictionary.insert("dazzling".to_string());
+    dictionary.insert("mellow".to_string());
+    dictionary.insert("breathe".to_string());
+    dictionary.insert("awesome".to_string());
+    dictionary.insert("tumble".to_string());
+    dictionary.insert("offend".to_string());
+    dictionary.insert("funny".to_string());
+    dictionary.insert("stew".to_string());
+    dictionary.insert("design".to_string());
+    dictionary.insert("control".to_string());
+
+    dictionary
 }
